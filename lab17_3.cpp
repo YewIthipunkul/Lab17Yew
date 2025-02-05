dbf11167a719ad92609484541dbbacee1ddab89d
Yew <yewit247@gmail.com>
2025-02-05 19:09:53 +0700

    finish

---
+++ lab17_3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+vector<int> randomVector(unsigned int N){	
+    vector<int> v;	
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> v){
+	cout << "[";
+	for(unsigned int i = 0; i<v.size(); i++){
+		cout << v[i];
+		if(i==v.size()-1) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> x, vector<int> y){
+	int sum = 0;
+	for(unsigned int i = 0; i<x.size(); i++) sum += x[i]*y[i];	
+	return sum;
+}
+
+int main(){
+	srand(time(0));
+	vector<int> x = randomVector(5);
+	vector<int> y = randomVector(5);
+	int z = dotProduct(x,y);
+	showVector(x);
+	cout << " . ";
+	showVector(y);
+	cout << " = " << z;
+	
+	return 0;
+}
